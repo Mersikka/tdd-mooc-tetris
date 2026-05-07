66741e1011438dff2438d29f12892c24d30a721b
mersikka <mio@argillander.me>
2026-05-07 21:00:01 +0300

    Add drop function

--- src/Board.mjs
+++ src/Board.mjs
@@ -1,13 +1,22 @@
 export class Board {
   width;
   height;
+  board;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
+    this.board = Array.from(
+      { length: this.height },
+      () => Array(this.width).fill("."));
   }
 
   toString() {
-    return Array(this.height).fill(Array(this.width).fill(".")).map( (x) => x.join("") ).join("\n") + "\n"
+    return this.board.map( (x) => x.join("") ).join("\n") + "\n"
+  }
+
+  drop(block) {
+    this.board[0][Math.floor(this.width / 2)] = block
+    return this
   }
 }
