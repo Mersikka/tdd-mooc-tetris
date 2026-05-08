f61f3d2194214dd616a00d9fd67c066adf069922
mersikka <mio@argillander.me>
2026-05-08 18:13:37 +0300

    Add newlines between functions

--- src/Tetromino.mjs
+++ src/Tetromino.mjs
@@ -2,11 +2,15 @@ import { RotatingShape } from '../src/RotatingShape.mjs';
 
 export class Tetromino {
   tetromino;
+
   constructor(tetrominoShape) {
     this.tetromino = RotatingShape.fromString(tetrominoShape)
   }
+
   toString() {
     return this.tetromino.toString()
   }
+
   static T_SHAPE = new Tetromino('.T.\nTTT\n...')
+
 };
