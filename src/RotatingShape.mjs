0c1df6b97fc191f1e8d8db17fc10a7032fc9fd3a
mersikka <mio@argillander.me>
2026-05-08 17:08:42 +0300

    Add clockwise rotation

--- src/RotatingShape.mjs
+++ src/RotatingShape.mjs
@@ -9,12 +9,12 @@ export class RotatingShape {
   toString() {
     return this.shape.map((line) => line.join('')).join('\n') + '\n'
   }
-//   rotateRight() {
-//     const newBoard = Array.from({ length: this.shape.length }, () => Array(this.shape.length).fill("."));
-//     this.shape[0].forEach((row, i) => {
-//       row.forEach((cell, j) => {
-//         newBoard[j][this.shape.length - i - 1] = cell
-//       })
-//     })
-//     return newBoard.map((line) => line.join()).join('\n') + '\n'}
+  rotateRight() {
+    const newBoard = Array.from({ length: this.shape.length }, () => Array(this.shape.length).fill("."));
+    this.shape.forEach((row, i) => {
+      row.forEach((cell, j) => {
+        newBoard[j][this.shape.length - i - 1] = cell
+      })
+    })
+    return newBoard.map((line) => line.join('')).join('\n') + '\n'}
 }
