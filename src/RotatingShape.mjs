6fa1507103a2daefec8d17d60991789e9cf41ec1
mersikka <mio@argillander.me>
2026-05-08 17:07:03 +0300

    Fix fromString and toString

--- src/RotatingShape.mjs
+++ src/RotatingShape.mjs
@@ -1,12 +1,20 @@
 export class RotatingShape {
   shape;
   constructor(shapeStr) {
-    this.shape = shapeStr.replaceAll(" ", "").split('\n').map((line) => line.split())
+    this.shape = shapeStr.replaceAll(" ", "").split('\n').map((line) => line.split(''))
   }
   static fromString(shapeStr) {
     return new RotatingShape(shapeStr)
   }
   toString() {
-    return this.shape.map((line) => line.join()).join('\n') + '\n'
+    return this.shape.map((line) => line.join('')).join('\n') + '\n'
   }
+//   rotateRight() {
+//     const newBoard = Array.from({ length: this.shape.length }, () => Array(this.shape.length).fill("."));
+//     this.shape[0].forEach((row, i) => {
+//       row.forEach((cell, j) => {
+//         newBoard[j][this.shape.length - i - 1] = cell
+//       })
+//     })
+//     return newBoard.map((line) => line.join()).join('\n') + '\n'}
 }
